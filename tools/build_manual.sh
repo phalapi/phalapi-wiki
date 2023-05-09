0eadfb5cf6bd5ef2280af61b6c8a1bdf2855d4d7
dogstar <huangchanzong@yesapi.cn>
2023-05-09 12:25:50 +0800

    类手册

---
+++ tools/build_manual.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+php ~/projects/github/phpDocumentor.phar -d ~/projects/github/kernal/src -f ~/projects/github/notorm/src/NotORM/Result.php -t ~/projects/github/phalapi-wiki/manual --title "PhalApi 2.21.6 类手册" --force
+
